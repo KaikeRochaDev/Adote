cf306503123b04543b6fa8a3476b298bf2ce1e1c
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-11 22:33:25 -0300

    list pet and add favicon

--- adote/settings.py
+++ adote/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios.apps.UsuariosConfig',
     'divulgar.apps.DivulgarConfig',
+    'adotar.apps.AdotarConfig',
 ]
 
 MIDDLEWARE = [
