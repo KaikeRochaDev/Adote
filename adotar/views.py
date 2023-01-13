f931e87ef7b94bcd3a2dfdb0e22cae699fa3bb0b
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-13 13:40:14 -0300

    see adoption requests

--- adotar/views.py
+++ adotar/views.py
@@ -4,6 +4,8 @@ from django.contrib.messages import constants
 from django.contrib import messages
 from .models import PedidoAdocao
 from datetime import datetime
+from django.http import HttpResponse
+from django.core.mail import send_mail
 def listar_pets(request):
     if request.method == 'GET':
         pets = Pet.objects.filter(status='P')
@@ -35,3 +37,27 @@ def pedido_adocao(request, id_pet):
 
     messages.add_message(request, constants.SUCCESS, 'Pedido de adoção realizado, você receberá um e-mail caso ele seja aprovado.')
     return redirect('/adotar')
+
+def processa_pedido_adocao(request, id_pedido):
+    status = request.GET.get('status')
+    pedido = PedidoAdocao.objects.get(id=id_pedido)
+    if status == 'A':
+        pedido.status = 'AP'
+        string = '''Olá sua adoção foi aprovada com sucesso...'''
+    elif status == 'R':
+        string = '''Olá, sua adoção foi recusada'''
+        pedido.status = 'R'
+        
+    pedido.save()
+    
+    #TODO: Alterar status do PET
+    
+    email = send_mail(
+        'Sua adoção foi processada',
+        string,
+        'kaike@gmail.com',
+        [pedido.usuario.email,]
+    )
+    
+    messages.add_message(request, constants.SUCCESS, 'Pedido de adoção processado com sucesso')
+    return redirect('/divulgar/ver_pedido_adocao')
