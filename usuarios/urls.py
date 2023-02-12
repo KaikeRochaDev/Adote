06e6aa5e9269cc240361fa62f1322e14920b497b
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-12 18:59:29 -0300

    styling buttons and fixing redirection

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.cadastro),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.logar, name='login'),
     path('sair/', views.sair, name="sair"),
